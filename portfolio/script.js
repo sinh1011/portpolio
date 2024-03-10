40be055d017815e25eb613ea0f7620f0c27b227b
sinh1011 <sinhtruongvo@gmail.com>
2024-03-10 12:11:34 +0700

    Your commit message

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var avatarImg = document.getElementById('avatarImg');
+    var infoDiv = document.getElementById('infoDiv');
+
+    avatarImg.style.opacity = '1';
+    infoDiv.style.opacity = '1';
+});
